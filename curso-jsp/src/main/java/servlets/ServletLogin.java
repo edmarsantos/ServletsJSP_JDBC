76098e68f8999b52598c99fe5b9965441b061b4d
edmarsantos <39056557+edmarsantos@users.noreply.github.com>
2021-09-20 21:29:16 -0300

    Criando o Model de Login
    Nessa aula foi criado a modem do login

---
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,76 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
+
+//também sao controlers Servlets
+@WebServlet("/ServletLogin") /*Mapeamento de URL que vem da tela*/
+public class ServletLogin extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+  
+    public ServletLogin() {
+    }
+
+
+    /*Recebe os dados pela url em parametros*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	
+	/*recebe os dados enviados por um formulario*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+			
+			
+			if(login !=null && !login.isEmpty() && senha!=null && !senha.isEmpty() ) {
+		
+
+				ModelLogin modelLogin = new ModelLogin();
+				modelLogin.setLogin(login);
+				modelLogin.setSenha(senha);
+		
+			}else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o Login e Senha corretamente!");
+				redirecionar.forward(request,response);
+			}
+		
+			
+			
+				
+			
+			
+			
+			/*
+			if (daoLogin.validarLogin(login, senha)) {
+				RequestDispatcher dispatcher = request.getRequestDispatcher("acessoliberado.jsp");
+				dispatcher.forward(request, response);
+			}else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher("acessonegado.jsp");
+				dispatcher.forward(request, response);
+			}}*/
+			
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		
+		
+	}
+
+}
--- curso-jsp/src/main/webapp/index.jsp
+++ curso-jsp/src/main/webapp/index.jsp
@@ -1,16 +1,35 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Curso JSP</title>
 </head>
 <body>
-<h1>Bem Vindo ao Curso de JSP</h1>
 
-<%
-	out.print("Sucesso Garantido");
-	%>
+	<h1>Bem Vindo ao Curso de JSP</h1>
+
+	
+	<form action="ServletLogin" method="post"  class="login-form">
+		 
+		 
+		 <table>
+		 <tr>
+		 <td>Login: </td>
+		 <td><input type="text" id="login" name="login"></td>
+		 </tr>
+		 <tr>
+		 <td>Senha:</td>
+		 <td> <input type="password" id="senha" name="senha"> </td>
+		 </tr>
+		 <tr>
+		 <td></td>
+		 <td><button type="submit" value="Logar">Logar</button> </td>
+		 </tr>
+		 </table>
+		 
+		 	</form>
+		 	<h4>${msg}</h4>
 </body>
 </html>
