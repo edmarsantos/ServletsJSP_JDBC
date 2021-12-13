0c81215e57cae9ca6af678e777a189d13ddac722
edmarsantos <39056557+edmarsantos@users.noreply.github.com>
2021-12-13 19:21:58 -0300

    Criando o DAOTelefoneRepository
    criando a classe e a daotelefoneRepository

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -318,6 +318,42 @@ public ModelLogin consultarUsuarioLogado(String login) throws Exception {
 		
 	}
 	
+public ModelLogin consultarUsuarioID(Long id) throws Exception {
+	
+	ModelLogin modelLogin = new ModelLogin();
+	
+	String sql = "select * from model_login where id = ? and useradmin is false";
+
+	PreparedStatement statement = connection.prepareStatement(sql);
+	statement.setLong(1, id);
+	
+	ResultSet resultado = statement.executeQuery();
+	
+		while(resultado.next()) {
+	
+		modelLogin.setId(resultado.getLong("id"));
+		modelLogin.setEmail(resultado.getString("email"));
+		modelLogin.setLogin(resultado.getString("login"));
+		modelLogin.setSenha(resultado.getString("senha"));
+		modelLogin.setNome(resultado.getString("nome"));
+		modelLogin.setPerfil(resultado.getString("perfil"));
+		modelLogin.setSexo(resultado.getString("sexo"));
+		modelLogin.setFotouser(resultado.getString("fotouser"));
+		modelLogin.setExtensaofotouser(resultado.getString("extensaofotouser"));
+		
+		modelLogin.setCep(resultado.getString("cep"));
+		modelLogin.setLogradouro(resultado.getString("logradouro"));
+		modelLogin.setBairro(resultado.getString("bairro"));
+		modelLogin.setLocalidade(resultado.getString("localidade"));
+		modelLogin.setUf(resultado.getString("uf"));
+		modelLogin.setSexo(resultado.getString("numero"));
+		
+		}
+	
+	return modelLogin;
+	
+}
+
 
 public ModelLogin consultarUsuarioID(String id,Long userLogado) throws Exception {
 	
